3d5b531c141ff743826d9c81491898fb9348d227
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-12 03:43:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash
+		programmer_hash[:yukihiro_matsumoto] = {
+		    :known_for => "Ruby",
+		    :languages => ["LISP", "C"]
+		  }
+		  return programmer_hash
 end
 
 def changing_alan
